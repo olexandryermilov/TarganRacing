3f38655d5b650c6b84cd33bde497867e50343631
olexandryermilov <olexandr.yermilov@gmail.com>
2017-03-07 22:28:41 +0200

    //added Stadium class, filled it

--- TarganRacing/Stadium.cpp
+++ TarganRacing/Stadium.cpp
@@ -1,12 +1,39 @@
 #include "Stadium.h"
+#include "json.hpp"
 
-
+using json = nlohmann::json;
 
 Stadium::Stadium()
 {
 }
 
+Stadium::Stadium(std::string name, int id, int qualityCoef, int length/*,std::string weatherType*/) :
+	name(name), id(id), qualityCoef(qualityCoef), length(length) {}
+
+Stadium::Stadium(const json& data)
+{
+	fromJson(data);
+}
 
 Stadium::~Stadium()
 {
+}
+
+json Stadium::toJson()
+{
+	json r;
+	r["name"] = name;
+	r["id"] = id;
+	r["qualityCoef"] = qualityCoef;
+	r["length"] = length;
+	return r;
+}
+
+void Stadium::fromJson(const json& data)
+{
+	//name = data["name"];
+	id = data["id"];
+	qualityCoef = data["qualityCoef"];
+	length = data["length"];
+	return;
 }
--- TarganRacing/Stadium.h
+++ TarganRacing/Stadium.h
@@ -1,8 +1,27 @@
 #pragma once
+#include "json.hpp"
+#include <string>
+
+using json = nlohmann::json;
 class Stadium
 {
 public:
 	Stadium();
+	Stadium(const json& data);
+	Stadium(std::string name, int id, int qualityCoef, int length/*,std::string weatherType*/);
 	~Stadium();
+	std::string getName() const { return name; }
+	int getId() const { return id; }
+	int getQuality() const { return qualityCoef; }
+	int getLength() const { return length; }
+	json toJson();
+	//std::string getWeather() const { return weatherType; }
+private:
+	std::string name;
+	int id;
+	int qualityCoef;
+	int length;
+	//std::string weatherType;
+	void fromJson(const json& data);
 };
 
