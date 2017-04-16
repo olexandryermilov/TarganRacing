ecd1937e1ca2b4279d908c8b0ebc512995ae1706
olexandryermilov <olexandr.yermilov@gmail.com>
2017-04-17 00:05:13 +0300

    //added main

---
+++ TarganRacing/Main.cpp
@@ -0,0 +1,23 @@
+#include "Stadium.h"
+#include "Racing.h"
+#include "BetCompany.h"
+#include "Cockroach.h"
+#include "TimeTable.h"
+#include "Gambler.h"
+
+
+int main()
+{
+	//generate cockroaches
+	//generate stadiums
+	//generate betcompanies
+	//generate gamblers
+	while (true)
+	{
+		//generate timetables
+		//assign coefs
+		//race
+		//take money
+		//update cockroaches
+	}
+}
