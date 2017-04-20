0d80c7baf1fa3bf03e29b6fe64a8f89e2407d0de
olexandryermilov <olexandr.yermilov@gmail.com>
2017-04-20 15:33:02 +0300

    //small

--- TarganRacing/Main.cpp
+++ TarganRacing/Main.cpp
@@ -4,11 +4,25 @@
 #include "Cockroach.h"
 #include "TimeTable.h"
 #include "Gambler.h"
+#include <vector>
 
+std::vector<Cockroach> getCockroaches(int cockroachesAmount)
+{
+	std::vector<Cockroach>result(cockroachesAmount);
+	for (int i = 0; i < cockroachesAmount; i++)
+	{
+		result[i] = Cockroach("Name"+(char)(i+'0'),i+1,"TeamName"+(char)(i+'0'));
+	}
+	return result;
+}
 
 int main()
 {
-	//generate cockroaches
+	const int cockroachesAmount = 9;
+	const int stadiumsAmount = 10;
+	const int betCompaniesAmount = 10;
+	const int gamblersAmount = 15;
+	std::vector<Cockroach>cockroaches = getCockroaches(cockroachesAmount); //generate cockroaches
 	//generate stadiums
 	//generate betcompanies
 	//generate gamblers
