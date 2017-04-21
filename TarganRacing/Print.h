253931c26d169e31b170cad0b1e4900f0f177a60
olexandryermilov <olexandr.yermilov@gmail.com>
2017-04-21 12:10:54 +0300

    Revert ".."
    This reverts commit cafbb202d70f4095d0e846fb7e16c193bd49f42f.

--- TarganRacing/Print.h
+++ TarganRacing/Print.h
@@ -1,23 +1,8 @@
 #pragma once
-#include <vector>
-#include "Cockroach.h"
-#include "Gambler.h"
-#include "Stadium.h"
-#include "BetCompany.h"
 class Print
 {
 public:
 	Print();
 	~Print();
-	void printAllCockroachesInfo(std::vector<Cockroach> &allCockroaches);
-	void printSingleCockroachInfo(Cockroach cockroach);
-	void printRaceInfo();
-	void printAllBetCompaniesInfo(std::vector<BetCompany>&allBetCompanies);
-	void printSingleBetCompanyInfo(BetCompany betCompany);
-	void printAllGamblersInfo(std::vector<Gambler>&allGamblers);
-	void printSingleGamblerInfo(Gambler gambler);
-	void printAllStadiumsInfo(std::vector<Stadium> &allStadiums);
-	void printSingleStadiumInfo(Stadium stadium);
-	void print
 };
 
